7efe1e9e5055fb767c16c50b90f5f3fc422b438a
DESKTOP-Q72REU7\math0 <math02840@gmail.com>
2021-06-08 14:20:16 +0200

    add route vue

--- nodedockerfront/app/src/App.vue
+++ nodedockerfront/app/src/App.vue
@@ -1,1 +1,32 @@
-HelloWorld
+<template>
+  <div id="nav">
+    Users : <br/>
+    <div class="list-unstyled" v-for="user in users" :key="user._id">
+      <li>
+        {{user.nom}} {{user.prenom}}
+      </li>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+const API_URL = "http://localhost:8080/getList";
+
+export default {
+  name: "home",
+  data: () => ({
+    error: "",
+    users: []
+  }),
+
+  mounted() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(result => {
+        this.users = result;
+      });
+  },
+  methods: {}
+};
+</script>
